fa5c19755982a65a8889f5af75c50326c8579bdf
Sam Mathew <sammathew@example.com>
2025-07-19 12:07:52 +0530

    Removed api and projectk folders from repository only

--- api/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from.models import*
-
-
-class StuForm(forms.ModelForm):
-    class Meta:
-        model=Stu
-        fields='__all__'
--- projectk/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from.models import*
-
-admin.site.register(Student)
-admin.site.register(Task)
-admin.site.register(Employee)
-admin.site.register(employeee)
